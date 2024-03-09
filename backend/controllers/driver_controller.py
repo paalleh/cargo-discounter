ddfe7eb0790b006ae110747c42c436b07905d37d
losiik <49565548+losiik@users.noreply.github.com>
2024-03-09 22:54:50 +0400

    feat(backend): add test rout

--- backend/controllers/driver_controller.py
+++ backend/controllers/driver_controller.py
@@ -5,3 +5,9 @@ from fastapi.exceptions import HTTPException
 driver_router = APIRouter(prefix='/driver')
 
 driver_router.tags = ["Driver"]
+
+
+@driver_router.post("/test", response_model=str)
+async def test(test_text: str):
+
+    return test_text
