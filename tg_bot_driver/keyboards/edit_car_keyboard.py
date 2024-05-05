b1fedaedf8faa634c74a39b78918e90026782634
losiik <49565548+losiik@users.noreply.github.com>
2024-05-05 21:42:54 +0400

    fix(driver_tg_bot): fix linter

--- tg_bot_driver/keyboards/edit_car_keyboard.py
+++ tg_bot_driver/keyboards/edit_car_keyboard.py
@@ -4,8 +4,7 @@ from aiogram import types
 kb = [
     [types.InlineKeyboardButton(
         text="Редактировать регистрационный номер автомобиля",
-        callback_data="edit_car_number")
-    ],
+        callback_data="edit_car_number")],
     [types.InlineKeyboardButton(text="Редактировать марку автомобиля", callback_data="edit_vendor")],
     [types.InlineKeyboardButton(text="Редактировать модель автомобиля", callback_data="edit_model")],
     [types.InlineKeyboardButton(text="Изменить тип кузов", callback_data="edit_load_capacity")],
